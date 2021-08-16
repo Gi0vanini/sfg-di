aeabce55869c9e73ec481b0a583787bf68c50e2e
Giovanni Marçon Rossi <giovanni.rossi@opus-software.com.br>
2021-08-16 10:19:12 -0300

    Adicionado controller de teste e feito hello world

--- src/main/java/guru/springframework/modulo2/Modulo2Application.java
+++ src/main/java/guru/springframework/modulo2/Modulo2Application.java
@@ -1,13 +1,21 @@
 package guru.springframework.modulo2;
 
+import guru.springframework.modulo2.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Modulo2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Modulo2Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(Modulo2Application.class, args);
+
+		MyController myController = (MyController)  ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
